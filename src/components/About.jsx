f82af0a9ac86fa114af892c04ce24f385514d06b
akshxy17 <youremail@example.com>
2024-12-06 11:20:53 +0530

    New BIO

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,19 +14,7 @@ const about = () => {
             <p className="uppercase text-2xl tracking-widest">about</p>
             <h2 className="py-2">Who I Am</h2>
             <p className="py-4 ">
-              Hello, My name is Akshay. I completed My
-              Bachelor of Technology at Malla Reddy Institute of Technology in
-              2023. In 2022, I began web development. I specialize in
-              developing responsive front-end UI applications that integrate
-              with APIs and other backend technologies. {text} enthusiastic
-              about learning new technologies and recognize that there are
-              multiple ways to complete a task. Though I am most comfortable
-              building front-end applications with HTML, CSS, Javascript, and
-              React, I am a quick learner and can quickly pick up new tech
-              stacks as needed. Being a great developer, In my opinion, is about
-              selecting the best tool for the job rather than using a specific
-              language. {text} now spending my time developing projects with
-              ReactJs and Firebase, as well as learning new technologies.
+            I am a 2023 B.Tech graduate from Malla Reddy Institute of Technology, specializing in building responsive front-end applications using React, HTML, CSS, and JavaScript. I focus on creating seamless integrations with APIs and backend technologies, always choosing the best tools for the job. Currently, I am working on projects with ReactJS and Firebase while continuously learning new technologies to enhance my skills.
             </p>
           </div>
           <div className="w-full h-auto m-auto bg-red-100 shadow-xl shadow-red-100 ease-in duration-300 rounded-full flex items-center justify-between p-4 hover:scale-105">
